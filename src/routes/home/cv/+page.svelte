074e42136b4344a3d391b8c3db2b99836d237190
Pierre Le Bras <7670356+plebras@users.noreply.github.com>
2023-09-09 14:27:10 +0100

    Add CV page, components and data

---
+++ src/routes/home/cv/+page.svelte
@@ -0,0 +1,26 @@
+<script>
+	import cv from '$lib/data/cv.json';
+
+	import CvRubric from './CVRubric.svelte';
+
+	function sortedRubrics() {
+		return cv
+			.map((r) => r)
+			.sort((a, b) => {
+				return a.order - b.order;
+			});
+	}
+</script>
+
+<h1>Curriculum</h1>
+<div id="cv">
+	{#each sortedRubrics() as rubric}
+		<CvRubric {rubric} />
+	{/each}
+</div>
+
+<style>
+	div#cv {
+		width: 100%;
+	}
+</style>
---
+++ src/routes/home/cv/CVRubric.svelte
@@ -0,0 +1,33 @@
+<script>
+	import CvEntry from './CVEntry.svelte';
+	import SectionTitle from '../SectionTitle.svelte';
+
+	import dateUtils from '$lib/utils/date.js';
+
+	export let rubric;
+
+	function sortedEntries() {
+		let count = 0;
+		return rubric.entries
+			.map((e) => {
+				e.id = count++;
+				return e;
+			})
+			.sort((a, b) => {
+				return dateUtils.comp(a.date, b.date);
+			});
+	}
+</script>
+
+<div class="rubric">
+	<SectionTitle title={rubric.rubric} />
+	{#each sortedEntries() as entry}
+		<CvEntry {entry} />
+	{/each}
+</div>
+
+<style>
+	div.rubric {
+		width: 100%;
+	}
+</style>
