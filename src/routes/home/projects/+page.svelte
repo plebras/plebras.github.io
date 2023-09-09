a5da748af8f1fa540a48fd650fb95e8f7377af62
Pierre Le Bras <7670356+plebras@users.noreply.github.com>
2023-09-09 16:11:23 +0100

    Add project page, components and data

---
+++ src/routes/home/projects/+page.svelte
@@ -0,0 +1,20 @@
+<script>
+	import projects from '$lib/data/projects.json';
+
+	import Project from './Project.svelte';
+
+	import { byYear } from '$lib/utils/date.js';
+</script>
+
+<h1>Projects</h1>
+<div id="projects">
+	{#each byYear(projects).flatMap((d) => d.values) as project}
+		<Project {project} />
+	{/each}
+</div>
+
+<style>
+	div#projects {
+		width: 100%;
+	}
+</style>
