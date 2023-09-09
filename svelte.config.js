2e8fdf2161671cccfa32e4329c5c2d0c2ade5541
Pierre Le Bras <7670356+plebras@users.noreply.github.com>
2023-09-09 17:22:44 +0100

    update svelte config

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,7 @@
 import adapter from '@sveltejs/adapter-static';
 
+const dev = process.argv.includes('dev');
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
@@ -7,11 +9,14 @@ const config = {
 			// default options are shown. On some platforms
 			// these options are set automatically — see below
 			pages: 'build',
-			assets: 'build',
-			fallback: 'index.html',
-			precompress: false,
-			strict: true
-		})
+			// assets: 'build',
+			// fallback: 'index.html',
+			// precompress: false,
+			// strict: true
+		}),
+		paths: {
+            base: dev ? '' : process.env.BASE_PATH,
+        }
 	}
 };
 
