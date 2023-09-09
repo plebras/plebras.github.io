f39511c8fa426a78bf3c3cdd1e1c9ee5632fc7af
Pierre Le Bras <7670356+plebras@users.noreply.github.com>
2023-09-09 17:26:45 +0100

    update svelte config

--- svelte.config.js
+++ svelte.config.js
@@ -10,7 +10,7 @@ const config = {
 			// these options are set automatically — see below
 			pages: 'build',
 			// assets: 'build',
-			// fallback: 'index.html',
+			fallback: 'index.html',
 			// precompress: false,
 			// strict: true
 		}),
